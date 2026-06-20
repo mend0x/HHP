ace5a61de2610097e1e4afdd51dfcb782e8c5a9d
mend0x <smendoexpo@gmail.com>
2026-06-21 00:48:37 +0200

    Rebuild docs with clean environment variables

--- docs/_next/static/OIYDGNky5Z5PTzuq6FPGh/_buildManifest.js
+++ docs/_next/static/OIYDGNky5Z5PTzuq6FPGh/_buildManifest.js
@@ -3,7 +3,7 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/HHP//_next/:path+",
+        "source": "/.//_next/:path+",
         "destination": "/_next/:path+"
       }
     ],
